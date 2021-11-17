5456cbbd63bc5515863745eb5f106079695d98ff
sergiaguilera <sergiaguileramorales@gmail.com>
2021-11-17 09:07:46 +0100

    metodo a base datos comentado

--- src/App.java
+++ src/App.java
@@ -1,4 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class App {
+    
+    private static final String JSONpath = "/Users/sergiaguileramorales/Desktop/proyectoIEI/proyectoIEI/archivosDatos/bibliotecas.json";
+
+    private static void ReadJSON(){
+
+        // JSONParser parser = new JSONParser();
+        // Object obj = parser.parse(new FileReader(JSONpath));
+        // JSONArray json = (JSONArray) obj;
+
+        // for (int i = 0; i < json.size(); i++) {
+
+        // JSONObject object =(JSONObject) json.get(i);
+        //ejemplos que hay q sustituir por nuestras columnas
+        // String clave = object.get("points").toString();  
+        // String title = object.get("title").toString();  
+        // System.err.println("points:"+clave);
+        // System.err.println("title:"+title);
+
+    }
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
